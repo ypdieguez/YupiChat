79cee7c1f4091fe4936e795f838ee171b3549569
Yordan P. Dieguez <ypdieguez@gmail.com>
2018-09-07 00:28:14 -0400

    Adding view to send message and JavaMail library. Work in progress.

--- app/src/main/java/com/github/sapp/yupi/MainActivity.kt
+++ app/src/main/java/com/github/sapp/yupi/MainActivity.kt
@@ -5,19 +5,59 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_conversation.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_compose_message.*
+import java.util.*
+import javax.mail.Message
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_conversation)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val adapter = ArrayAdapter.createFromResource(this, R.array.countries_code,
+                android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        codeCountrySpinner.adapter = adapter
+
+        sendMsgBtn.setOnClickListener {
+            if(!numberEditText.text.isEmpty() && !composeMessageText.text.isEmpty()) {
+                Thread(Runnable { try {
+                    val phone = StringBuilder(codeCountrySpinner.selectedItem.toString())
+                            .append(numberEditText).toString()
+//                    Toast.makeText(this, phone, Toast.LENGTH_LONG).show()
+
+                    val props = Properties()
+                    props["mail.smtp.host"] = "smtp.gmail.com"
+                    props.setProperty("mail.smtp.ssl.enable", "true")
+
+                    val session = Session.getInstance(props)
+
+                    val msg = MimeMessage(session)
+                    msg.setFrom()
+                    msg.setRecipients(Message.RecipientType.TO,
+                            InternetAddress.parse("ypdieguez@gmail.com", false))
+                    msg.subject = StringBuilder(codeCountrySpinner.selectedItem.toString())
+                            .append(numberEditText).toString()
+                    msg.setText(composeMessageText.text.toString())
+                    msg.setHeader("X-Mailer", "Yuuupi")
+                    msg.sentDate = Date()
+
+                    Transport.send(msg, "gtom20180828@gmail.com", "GtoM_2018/08/28")
+                } catch (e: Exception) {
+                    val a = 1
+                }
+                }).start()
+            }
         }
     }
 
